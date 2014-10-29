27074fad12e4996ab0d5a7c946f27f7830a39233
pankaj_pandey <pankaj_pandey@apple.com>
2014-10-29 20:17:42 +0530

    <rdar://problem/18662826> Add/Edit invoice changes - implemented review comments

--- models/fixtures/fixtures.js
+++ models/fixtures/fixtures.js
@@ -14,9 +14,7 @@ fixture('GET /contentType', '/models/fixtures/contentType.json');
 
 fixture('GET /getInvoiceById/1024', '/models/fixtures/getInvoiceById.json');
 
-<<<<<<< HEAD
 fixture('GET /fxrate', '/models/fixtures/fxrate.json');
-=======
 fixture('GET /global-licensors', '/models/fixtures/global-licensor.json');
 
 fixture('GET /global-regions', '/models/fixtures/global-region.json');
@@ -24,5 +22,3 @@ fixture('GET /global-regions', '/models/fixtures/global-region.json');
 import fixture_country from './global-country';
 
 fixture('GET /global-storeTypes', '/models/fixtures/global-store-type.json');
->>>>>>> 49cb2f29b3a0d41f730f5baba112f2f9e675effe
-
