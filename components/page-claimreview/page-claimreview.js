4025a3fd90c8b02f264e3330bff5c286f29c61c3
Kumaresh <kumaresh_mk@apple.com>
2014-11-01 18:59:29 +0530

    <rdar://problem/18663052 > Claim Review page added

---
+++ components/page-claimreview/page-claimreview.js
@@ -0,0 +1,12 @@
+import Component from 'can/component/';
+
+import template from './template.stache!';
+import styles from './page-claimreview.less!';
+
+var page = Component.extend({
+  tag: 'page-claimreview',
+  template: template,
+  scope: {}
+});
+
+export default page;
