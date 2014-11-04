6ff5098e8586b988520ef891ba4cd32708fc5fa5
pankaj_pandey <pankaj_pandey@apple.com>
2014-11-04 19:19:57 +0530

    <rdar://problem/18662942> ICSV changes- Added bottom grid.

--- components/page-icsv/page-icsv.js
+++ components/page-icsv/page-icsv.js
@@ -1,4 +1,9 @@
 import Component from 'can/component/';
+import View from 'can/view/';
+import _ from 'lodash';
+
+import Grid from 'components/grid/';
+import stache from 'can/view/stache/';
 
 import template from './template.stache!';
 import styles from './page-icsv.less!';
@@ -6,28 +11,131 @@ import topfilter from 'models/sharedMap/topfilter';
 import dataTables from 'components/data-tables/';
 import icsvmap from 'models/sharedMap/icsv';
 
+Grid.extend({
+  scope: {
+    appstate:undefined,
+    columns: [
+      {
+        id: 'id',
+        title: 'id'
+      },
+      {
+        id: 'error',
+        title: 'error'
+      },
+      {
+        id: 'licensor',
+        title: 'Licensor'
+      },
+      {
+        id: 'invoiceCategory',
+        title: 'Invoice Category'
+      },
+      {
+        id: 'contentType',
+        title: 'Content Type'
+      },
+      {
+        id: 'country',
+        title: 'Country'
+      },
+      {
+        id: 'invoiceNum',
+        title: 'Invoice No'
+      },
+      {
+        id: 'dueDate',
+        title: 'Due date'
+      },
+      {
+        id: 'invoiceAmt',
+        title: 'Amount'
+      },
+      {
+        id: 'currency',
+        title: 'Currency'
+      },
+      {
+        id: 'comments',
+        title: 'User comments'
+      }
+    ]
+  }
+});
+
+
 var page = Component.extend({
   tag: 'page-icsv',
   template: template,
   scope: { 
-  		
+      //summaryData:{},
+      invoicesData:{}
+    },
+    init:function(){
+    topfilter.attr("show",false);
+      var self = this; 
+      self.scope.attr("invoicesData", icsvmap.invoicedata);
+     
+     /* icsvmap.bind('uploadiCSV', function(ev, newVal, oldVal) {
+         self.scope.attr("uploadData", newVal);
+
+         Promise.all([
+          //AllInvoices.findAll({region: 2})
+          Validateinvoice.findAll(newVal)
+        ]).then(function(values) {
+          //console.log(JSON.stringify(values[0][1]));
+          if(values[0][0]["responseHeader"]["errorCode"]=="0000")
+            icsvmap.attr('icsvmap','values');
+            //self.scope.invoicesMap.replace(values[0][1]);
+        });
+    }); */
     },
    events:{
   		"inserted":function(){
-  			topfilter.attr("show",false);
-      }
-  	 },
-  	init:function(){
+        var self = this;
+        /*icsvmap.bind('icsvdata', function(ev, newVal, oldVal) { //This should be used when icsvdata changed by upload component
+             //this.scope.summarydata = newval.summaryDat
+             this.scope.invoicesData = newval.invoices;
+             console.log(newval.invoices);
+         }); */
+          
+        var gridData = [];
 
-  		  /*Promise.all([
-		     	//AllInvoices.findAll({region: 2})
-		     	AllInvoices.findAll()
-		    ]).then(function(values) {
-		    	//console.log(JSON.stringify(values[0][1]));
-		    	if(values[0][0]["responseHeader"]["errorCode"]=="0000")
-		     		//self.scope.invoicesMap.replace(values[0][1]);
-		    });*/
-		}
+
+        console.log(self.scope.invoicesData.attr()[0].invoices);
+
+        var tempArr = self.scope.invoicesData.attr()[0].invoices;
+        
+        for(var i=0; i< tempArr.length; i++){
+          var tempObj = {};
+          tempObj.id= tempArr[i].invoiceNumber;
+          tempObj.error = "dqweq";
+          tempObj.licensor= tempArr[i].entityName;
+          tempObj.invoiceCategory= "invoiceCategory";  /* invoice category not in sample data*/
+          tempObj.contentType= "contentType"; /*need to take count from from invoice line*/
+          tempObj.country= "country"; /*need to take count from from invoice line*/
+          tempObj.invoiceNum= tempArr[i].invoiceNumber;
+          tempObj.dueDate= tempArr[i].invoiceDueDate;
+          tempObj.invoiceAmt= tempArr[i].invoiceAmount;
+          tempObj.currency= tempArr[i].invoiceCcy;
+          tempObj.comments= tempArr[i].comments[0].comments;
+         // tempObj.comments= "fwfee";
+          gridData.push(tempObj);
+        }
+
+
+       // var gridData = [{id:"1", error:"error", licensor:"lic"},{id:"1", error:"error", licensor:"lic"}];
+        var rows = new can.List(gridData);
+       $('#icsvinvoiceGrid').html(stache('<rn-grid rows="{rows}"></rn-grid>')({rows})); 
+      },
+      "{invoicesData} change":function(){
+
+        
+
+      }
+    }
+
+
 });
 
 export default page;
--- models/sharedMap/icsv.js
+++ models/sharedMap/icsv.js
@@ -1,107 +1,91 @@
 import Map from 'can/map/';
 import can_define from 'can/map/define/';
+import _ from 'lodash';
+import Grid from 'components/grid/';
 
-var icsvObj = {invoiceid: '',
-icsv: {"invoice": [
+var icsvObj = {
+    invoicedata:[
     {
-        "serviceTypeId": "1",
-        "invoiceNumber": "inv2510",
-        "invoiceTypeId": "",
-        "entityId": "40",
-        "entityName": "NCB",
-        "invoiceCcy": "DKK",
-        "fxRate": 1,
-        "notes": "Notes from licensors",
-        "invoiceAmount": "2000",
-        "tax": "56",
-        "grossTotal": "2056",
-        "receivedDate": "06/19/2014",
-        "invoiceDate": "06/19/2014",
-        "invoiceCalculatedDueDate": "",
-        "invoiceDueDate": "09/19/2014",
-        "comments": [
-                {
-                    "comments": "iCSVGeneratedInvoice",
-            		"createdBy": "",
-            		"createdDate": ""
-		        }		
-            ],
-		"documents": [
-		   {
-    		    "fileName": "file.csv",
-                "location": "/path"
+        "responseCode": "SUCCESS",
+        "responseText": "Records returned successfully.",
+        "summary": {
+            "fileCount": "5",
+            "invoiceCount": "8",
+            "pdfCount": "4",
+            "licensorCount": "2",
+            "rowCount": "10",
+            "errorCount": "20",
+            "invoiceTotalMap": {
+                "EUR": 20000,
+                "DKK": 10000
             }
-        ],
-        "invoiceLines": [
+        },
+        "invoices": [
             {
-                "countryId": "DKK",
-                "fiscalPeriod": "201306",
-                "periodType": "Q",
-	            "contentTypeId": "1",
-        		"contentType": "Music",
-        		"lineAmount": "2000.00",
-        		"ccidName": "detailFileNameFromIcsv.DAT",
-        		"errors":[
-                	{
-                		"status":"",
-                		"errorMap":[{}]
-                	}	
+                "serviceTypeId": "1",
+                "invoiceNumber": "inv2509",
+                "invoiceTypeId": "",
+                "entityId": "",
+                "entityName": "",
+                "invoiceCcy": "DKK",
+                "fxRate": 1,
+                "notes": "Notes from licensors",
+                "invoiceAmount": "2000",
+                "tax": "56",
+                "grossTotal": "",
+                "receivedDate": "06/19/2014",
+                "invoiceDate": "06/19/2014",
+                "invoiceCalculatedDueDate": "",
+                "invoiceDueDate": "09/19/2014",
+                "comments": [
+                    {
+                        "comments": "This is a Regular Invoice comment",
+                        "createdBy": "",
+                        "createdDate": ""
+                    }
+                ],
+                "documents": [
+                    {
+                        "fileName": "file.csv",
+                        "location": "/path"
+                    }
+                ],
+                "invoiceLines": [
+                    {
+                        "countryId": "DNK",
+                        "fiscalPeriod": "201306",
+                        "periodType": "Q",
+                        "contentTypeId": "",
+                        "contentType": "Music",
+                        "lineAmount": "2000.00",
+                        "ccidName": "detailFileNameFromIcsv.DAT",
+                        "adhocTypeId": "",
+                        "glAccRefId": "",
+                        "errors": [
+                            {
+                                "status": "",
+                                "errorMap": [
+                                    {}
+                                ]
+                            }
+                        ]
+                    }
+                ],
+                "errors": [
+                    {
+                        "status": "",
+                        "errorMap": [
+                            {}
+                        ]
+                    }
                 ]
             }
-        ]       
-   },
-   {
-        "serviceTypeId": "1",
-        "invoiceNumber": "inv2509",
-        "invoiceTypeId": "",
-        "entityId": "40",
-        "entityName": "NCB",
-        "invoiceCcy": "DKK",
-        "fxRate": 1,
-        "notes": "Notes from licensors",
-        "invoiceAmount": "2000",
-        "tax": "56",
-        "grossTotal": "2056",
-        "receivedDate": "06/19/2014",
-        "invoiceDate": "06/19/2014",
-        "invoiceCalculatedDueDate": "",
-        "invoiceDueDate": "09/19/2014",
-        "comments": [
-                {
-                    "comments": "iCSVGeneratedInvoice",
-            		"createdBy": "",
-            		"createdDate": ""
-		        }		
-            ],
-		"documents": [
-		   {
-    		    "fileName": "file.csv",
-                "location": "/path"
-            }
-        ],
-        "invoiceLines": [
-            {
-                "countryId": "DKK",
-                "fiscalPeriod": "201306",
-                "periodType": "Q",
-	            "contentTypeId": "1",
-        		"contentType": "Music",
-        		"lineAmount": "2000.00",
-        		"ccidName": "detailFileNameFromIcsv.DAT",
-        		"errors":[
-                	{
-                		"status":"",
-                		"errorMap":[{}]
-                	}	
-                ]
-            }
-        ]       
-   }
-   
- ]
+        ]
+    }
+ ],
+uploadiCSV:{
+
 }
-
-
 },
 icsvMap = new can.Map(icsvObj);
 
