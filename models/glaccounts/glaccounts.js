550a0440db50e919ecd4c4bfd438fbfa1f06f4e1
naveen_c <naveen_c@apple.com>
2014-11-12 20:39:03 +0530

    <rdar://problem/18939711> Integration of UI Services with UI interface

--- models/glaccounts/glaccounts.js
+++ models/glaccounts/glaccounts.js
@@ -2,7 +2,15 @@ import Model from 'can/model/';
 import RinsCommon from 'utils/';
 
 var GLaccounts = Model.extend({
-        findAll: 'POST ' +  RinsCommon.UI_SERVICE_URL+'getGlAccounts'
-    }, {});
+	findAll: function(params){
+	 	return $.ajax({
+	 		url: RinsCommon.UI_SERVICE_URL +'getGlAccounts',
+	  		type: 'POST',
+	  		data: JSON.stringify(params),
+	  		dataType:'json',
+	  		contentType: 'application/json'
+	  	});
+	}
+	}, {});
 
 export default GLaccounts;
