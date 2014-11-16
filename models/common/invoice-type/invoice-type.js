57e9a3842f057fd8de729884b6d30e32129e43d9
pankaj_pandey <pankaj_pandey@apple.com>
2014-11-16 19:27:50 +0530

    <rdar://problem/18662826> Add/Edit review changes

--- models/common/invoice-type/invoice-type.js
+++ models/common/invoice-type/invoice-type.js
@@ -4,7 +4,7 @@ import RinsCommon from 'utils/';
 
 
 var InvoiceType = Model.extend({
-    findAll: function(params){
+  /*  findAll: function(params){
      	return $.ajax({
      		contentType: 'application/json; charset=utf-8',
       		url: RinsCommon.UI_SERVICE_URL +'getInvoiceTypes',
@@ -12,7 +12,10 @@ var InvoiceType = Model.extend({
       		data: JSON.stringify(params),
       		dataType:'json'
       	});
-    }
+    }*/
+
+findAll: "GET /invoiceType"
+
 }, {});
 
 export default InvoiceType;
