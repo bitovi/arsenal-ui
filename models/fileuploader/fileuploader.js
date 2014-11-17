b8a6a421f1f3a3c96d5775c01d7a8daf3d4bff9b
naveen_c <naveen_c@apple.com>
2014-11-17 17:20:57 +0530

    <rdar://problem/18939711> Integration of UI Services with UI interface

---
+++ models/fileuploader/fileuploader.js
@@ -0,0 +1,19 @@
+import Model from 'can/model/';
+import RinsCommon from 'utils/';
+
+var FileUpLoader = Model.extend({
+
+    create: function(params) {
+
+    	return $.ajax({
+    		            url: RinsCommon.UI_SERVICE_URL+'/uploadFiles',
+	    							type: 'POST',
+	    							contentType: 'multipart/form-data;boundary=' + RinsCommon.BOUNDARY +';' ,
+	    							processData: false,
+	    							data: params
+									});
+
+				}
+
+ 		} ,{});
+	export default FileUpLoader;
