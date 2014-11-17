6ad7e57be93536b25d71296fc6ef6d0a0bdd4c56
Kumaresh <kumaresh_mk@apple.com>
2014-11-17 11:55:59 +0530

    <rdar://problem/18662923 > GetAllInvoices- UI changes

--- components/grid/grid.js
+++ components/grid/grid.js
@@ -95,6 +95,42 @@ var Grid = Component.extend({
         });
       });
     },
+    footerRows: function(options) {
+      // By default, rows are a bit more complex.
+      // We have to account for child rows being invisible when their parents aren't open.
+      //console.log("Footer Rows are "+JSON.stringify(this.footerrows.attr()));
+      var isRowAChild = function(row) {
+        // by default, just looking for __isChild = true
+        return !!row.attr('__isChild');
+      };
+
+      var isRowOpen = function(row) {
+        // by default, just looking for __isOpen = true
+        return !!row.attr('__isOpen');
+      };
+
+      var out = [],
+          childRowsAreVisible = false,
+          scope = this;
+
+      this.footerrows.attr('length');
+      return _.map(this.footerrows, function(row) {
+        var isChild = isRowAChild(row);
+
+        // if the row is a parent and isn't open, its children shouldn't be visible -
+        // this flag is only true for the children of an open parent row
+        if(!isChild) {
+          childRowsAreVisible = isRowOpen(row);
+        }
+
+        return options.fn({
+          row: row,
+          isOpen: isChild ? false : isRowOpen(row), // child rows are never open
+          isChild: isChild,
+          isVisible: isChild ? childRowsAreVisible : true // parent rows are always visible
+        });
+      });
+    },
     rowClass: function(row) {
       // By default, just return nothing.
       return '';
