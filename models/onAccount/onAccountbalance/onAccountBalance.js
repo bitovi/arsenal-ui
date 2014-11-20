656ffb3c4c2d9e91969eddb91755acd71f9eaacb
naveen_c <naveen_c@apple.com>
2014-11-20 17:58:34 +0530

    <rdar://problem/18662957> On-Account / New On Account - UI Interface - Construct View with other functionalities

---
+++ models/onAccount/onAccountbalance/onAccountBalance.js
@@ -0,0 +1,8 @@
+import Model from 'can/model/';
+
+var Licensor = Model.extend({
+  // using finaAll instead of resource because I don't want to be able to save.
+  findAll: 'GET /onAccountBalance'
+}, {});
+
+export default Licensor;
