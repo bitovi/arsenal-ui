f0f76f3571fa7dd1be5256ba9fe987edfd5193fc
ajith <analla@apple.com>
2014-11-25 18:27:51 -0800

    rdar://problem/18663002 added initial set - Ingested tab

--- components/page-recon-stats/page-recon-stats.js
+++ components/page-recon-stats/page-recon-stats.js
@@ -9,15 +9,30 @@ import ReconStats from 'components/recon-stats/';
 var page = Component.extend({
   tag: 'page-recon-stats',
   template: template,
-  
+
   scope: {
+    tabSelected :""
 
-      
-  	},
+  },
 
-  
+  helpers: {
+
+    isRejectBtn:function(pageName){
+      //TODO based on the selected rows
+      return (pageName === this.attr('tabSelected') ? 'disabled' : '' ) ;
+      //return 'style="display:' + (pageName === this.attr('tabSelected') ? 'block' : 'none') + '"';
+    }
+
+  },
+  events:{
+    'shown.bs.tab': function(el, ev) {
+        this.scope.attr("tabSelected", $('.nav-tabs .active').text());
+
+       console.log(this.scope.attr("tabSelected"));
+    }
+  }
+
 });
 
 
 export default page;
-
