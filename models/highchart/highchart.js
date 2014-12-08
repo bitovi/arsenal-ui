c7a15a95099969578f43ed76b2a6e15e92eb5b42
naveen_c <naveen_c@apple.com>
2014-12-08 18:26:07 +0530

    trying to rebase

--- models/highchart/highchart.js
+++ models/highchart/highchart.js
@@ -1,4 +1,5 @@
 import Model from 'can/model/';
+<<<<<<< HEAD
 import RinsCommon from 'models/rinsCommon/';
 
 var highchart = Model.extend({
@@ -11,6 +12,11 @@ findOne: function(params){
   		contentType: 'application/json'
   	});
 }
+=======
+
+var highchart = Model.extend({
+ findAll: 'GET /highchart'
+>>>>>>> <rdar://problem/18718561> UI Interface -Historical Graphs PBR/CR
 }, {});
 
 export default highchart;
