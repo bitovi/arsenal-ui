383632573d3085ca1e5d6d11231b7fd052d1492b
naveen_c <naveen_c@apple.com>
2014-12-09 10:15:02 +0530

    Resolved the compilation error in common components

---
+++ models/common/content-type/content-type.js
@@ -0,0 +1,19 @@
+import Model from 'can/model/';
+import RinsCommon from 'utils/';
+
+var ContentType = Model.extend({
+	findAll: function(params){
+	 	return $.ajax({
+	 		url: RinsCommon.UI_SERVICE_URL +'getContentTypes',
+	  		type: 'POST',
+	  		data: JSON.stringify(params),
+	  		dataType:'json',
+	  		contentType: 'application/json'
+	  	});
+	}
+	}, {});
+
+
+/* able to get data in ajax done function*/
+
+export default ContentType;
---
+++ models/common/licensor/licensor.js
@@ -0,0 +1,21 @@
+import Model from 'can/model/';
+
+import RinsCommon from 'utils/';
+
+var Licensor = Model.extend({
+    findAll: function(params){
+     	return $.ajax({
+      		url: RinsCommon.UI_SERVICE_URL +'getLicensors',
+      		//url: 'http://17.149.230.135:10645/getLicensors',
+      		type: 'POST',
+      		data: JSON.stringify(params),
+      		dataType:'json',
+      		contentType: 'application/json'
+      	});
+    }
+}, {});
+
+
+/* able to get data in ajax done function*/
+
+export default Licensor;
