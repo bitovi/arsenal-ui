d59a3b1ddb3df88c66966195d65280e454008b1d
Sudesna Maharathy <sudesna_maharathy@apple.com>
2014-12-08 21:34:48 -0800

    <rdar://problem/18664368> UI Interface - Navigation and Menu

--- models/appstate/appstate.js
+++ models/appstate/appstate.js
@@ -6,39 +6,47 @@ import can_pushstate from 'can/route/pushstate/';
 
 var State = Map.extend({
 
-  define: {
-    page: {
-      type: 'string'
-    }
-  },
+    define: {
+        page: {
+            type: 'string'
+        }
+    },
 
-  /*
-   * Override can.Map.serialize.
-   * This produces a URL-friendly export of the app state's properties.
-   * @returns {{ page: String }}
-   */
-  serialize: function() {
-    return {
-      page: this.attr('page')
-    };
-  }
+    /*
+     * Override can.Map.serialize.
+     * This produces a URL-friendly export of the app state's properties.
+     * @returns {{ page: String }}
+     */
+    serialize: function() {
+        return {
+            page: this.attr('page')
+        };
+    }
 
 });
 
 var stateDefaults = {
-  page: 'dashboard',
-  licensor: undefined,
-  region: undefined,
-  storeType: undefined
+    page: 'dashboard',
+    periodFrom: undefined,
+    periodTo: undefined,
+    licensor: undefined,
+    country: undefined,
+    region: undefined,
+    storeType: undefined,
+    renderGlobalSearch:false,
+    globalSearch:undefined
 };
 
+
+
 var appState = new State({
-  startRouting: function() {
-    // Create a route and pass in initial values.
-    route(':page', stateDefaults);
-    // start routing!
-    route.ready();
-  }
+    startRouting: function() {
+        // Create a route and pass in initial values.
+        route(':page', stateDefaults);
+
+        // start routing!
+        route.ready();
+    }
 });
 
 // Make `appState` the Map that can.route uses to keep its data in.
