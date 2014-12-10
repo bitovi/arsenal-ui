b5b8bbf13ef424c0d55067c2288b41ff2e9ca237
Kumaresh <kumaresh_mk@apple.com>
2014-12-10 18:48:15 +0530

    Claim review - inclusion of actual web service

--- models/claim/licensor/licensor.js
+++ models/claim/licensor/licensor.js
@@ -1,17 +1,14 @@
 import Model from 'can/model/';
-import RinsCommon from 'models/rinsCommon/';
+import RinsCommon from 'utils/';
 
 var claimLicensorInvoices = Model.extend({
-  // using findAll instead of resource because I don't want to be able to save.
-  //findAll: 'GET /claimLicensorInvoices'
   findOne: function(params){
     return $.ajax({
-      url: RinsCommon.DOMAIN_SERVICE_URL+'claims/get/entity',
-      //url: RinsCommon.DOMAIN_SERVICE_URL+'rinsui/getClaimReviews',
+      url: RinsCommon.DOMAIN_SERVICE_URL+'rinsui/getClaimReviews',
       type: 'POST',
       datatype:'json',
-      //contentType: 'application/json; charset=utf-8',
-      //data: JSON.stringify(params)
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(params)
     })
   }
 }, {});
