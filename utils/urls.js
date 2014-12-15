14c3bfda2e89e3f43d173fa6dbaaedb390419541
naveen_c <naveen_c@apple.com>
2014-12-15 18:39:19 +0530

    Add the default value for the period from and period to

--- utils/urls.js
+++ utils/urls.js
@@ -1,5 +1,5 @@
 var URLs = {
-  UI_SERVICE_URL:"http://localhost:10645/api/v1/rinsui/",
+  UI_SERVICE_URL:"http://ma-rinst-lap01.corp.apple.com:10645/api/v1/rinsui/",
   DOMAIN_SERVICE_URL:"http://ma-rinst-lap01.corp.apple.com:10639/api/v1/",
   INTEGRATION_FILE_MANGER_URL: "http://ma-rinst-lap01.corp.apple.com:8082/api/v1/rinsfilemanager/",
   RINS_OLD_URL:"http://rightnotes-uat.corp.apple.com/"
