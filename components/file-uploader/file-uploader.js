e1eb29f266faa1037ada18924ac4510812596487
ctsuser1 <ctsuser1@static-243.105.99.14-tataidc.co.in>
2014-12-16 19:53:42 +0530

    <rdar://problem/18718250> Create Invoices from iCSV - UI Interface - Construct View(Upper Grid) with other funstionalities also fixed the some of the onAccount

--- components/file-uploader/file-uploader.js
+++ components/file-uploader/file-uploader.js
@@ -12,6 +12,7 @@
                   fileList : new can.List(),
                   displayMessage:"display:none",
                   fileUpload:false,
+                  uploadedfileinfo:[],
                   isAnyFileLoaded : can.compute(function() { return this.fileList.attr('length') > 0; })
 
               },
@@ -30,13 +31,19 @@
                     this.scope.fileList.push.apply(this.scope.fileList, files);
                   },
                  '.submitFiles click': function() {
-                    var self = this.scope;
+                    var self = this;
                     var size = this.scope.attr("fileList").length;
                     FileUpLoader.create(this.scope.attr("fileList"),function(data) {
-                    self.attr("fileUpload" , true);
-                    self.attr("displayMessage","display:block");
-                    self.attr("fileList").splice(0,size);
-                    console.log("Success")
+                    self.scope.attr("fileUpload" , true);
+                    var response = data.filePropeties[0];
+                    if(response.status=='SUCCESS'){
+                      /*passing file length here to parent component*/
+                       $('.success').empty().html(response.responseText);
+                      $(self.element).trigger('onSelected', data);
+                    }else{
+                       $('.fileError').empty().html(response.responseText);
+                    }
+                    //console.log("Success")
                     },function(xhr) {
                       console.error("Error while loading:"+xhr);
                     });
@@ -44,10 +51,23 @@
                 '.cancelUpload click': function() {
                     var size = this.scope.attr("fileList").length;
                     this.scope.attr("fileList").splice(0,size);
+                     var uploadedfileinfoSize = this.scope.uploadedfileinfo.length;
+                    this.scope.uploadedfileinfo.splice(0,uploadedfileinfoSize);
                 },
                 '.filelist li click': function(el, ev) {
-                    var selectedIndex = this.scope.attr('fileList').indexOf(el.data('file'));
-                    this.scope.attr("fileList").splice(selectedIndex,1);
+                   var liText = el.text();
+                    var index = liText.indexOf("(");
+                    var name = '';
+                    var selectedIndex = '';
+                    if(index != -1) {
+                      name = liText.substring(0,index);
+                      this.scope.attr("fileList").forEach(function(file, index) {
+                          if(file.name == name.trim()) {
+                            selectedIndex = index;
+                          }
+                      });
+                    }
+                      this.scope.attr("fileList").splice(selectedIndex,1);
                 }
             }
       })
