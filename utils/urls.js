4cb28ae2b38b8cbd353d7623f258ca8ef9824082
Eli Morris-Heft <eli_morrisheft@apple.com>
2014-12-16 09:35:20 -0600

    Added email server URL.

--- utils/urls.js
+++ utils/urls.js
@@ -3,7 +3,8 @@ var URLs = {
   DOMAIN_SERVICE_URL:"http://ma-rinst-lap01.corp.apple.com:10639/api/v1/",
   INTEGRATION_SERVICE_URL: "http://ma-rinst-lap01.corp.apple.com:8084/api/v1/",
   FILE_MANAGER_SERVICE_URL: "http://ma-rinst-lap01.corp.apple.com:8082/api/v1/rinsfilemanager/",
-  RINS_OLD_URL:"https://rightnotes-uat.corp.apple.com/"
+  EMAIL_SERVICE_URL: "http://ma-rinst-lap01.corp.apple.com:8086/api/v1",
+  RINS_OLD_URL:"http://rightnotes-uat.corp.apple.com/"
 };
 
 export default URLs;
