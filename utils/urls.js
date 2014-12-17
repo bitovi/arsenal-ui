d19a3d46c5fc25ed3dbed36930d4a969c9700051
ajith <analla@apple.com>
2014-12-16 16:34:41 -0800

    <rdar://problem/19255813> UI - Recon stats - Refresh stats not working, updated URL

--- utils/urls.js
+++ utils/urls.js
@@ -2,7 +2,7 @@ var URLs = {
   UI_SERVICE_URL:"http://ma-rinst-lap01.corp.apple.com:10645/api/v1/rinsui/",
   DOMAIN_SERVICE_URL:"http://ma-rinst-lap01.corp.apple.com:10639/api/v1/",
   INTEGRATION_SERVICE_URL: "http://ma-rinst-lap01.corp.apple.com:8084/api/v1/",
-  FILE_MANAGER_SERVICE_URL: "http://localhost:8082/api/v1/rinsfilemanager/",
+  FILE_MANAGER_SERVICE_URL: "http://ma-rinst-lap01.corp.apple.com:8082/api/v1/rinsfilemanager/",
   RINS_OLD_URL:"http://rightnotes-uat.corp.apple.com/"
 };
 
