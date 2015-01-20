fd82bdfd99a999a34550c780933ddd35fbdd83ee
ajith <analla@apple.com>
2015-01-20 01:42:29 -0800

    <rdar://problem/19455957> UI - PBR - Workflow order incorrect

--- models/workflow-step/workflow-step.js
+++ models/workflow-step/workflow-step.js
@@ -4,8 +4,8 @@ import URLs from 'utils/urls';
 
 var WorkflowStep = Model.extend({
   id: 'stepId',
-  parseModels: 'workflowView.nodes',
-  findAll: function(params) {
+  //parseModels: 'workflowView.nodes',
+  loadWorkFlowView: function(params) {
     return $.ajax({
       url: URLs.UI_SERVICE_URL + 'view',
       type: 'POST',
