feb62dac3d1b24ca29e7b892fb1285a9250ed249
pankaj_pandey <pankaj_pandey@apple.com>
2015-01-20 15:32:34 +0530

    <rdar://problem/19527188> UI : Approvals dashboard scrolling grid and PBR navigation implementation missing

--- components/email-confirm-modal/email-confirm-modal.js
+++ components/email-confirm-modal/email-confirm-modal.js
@@ -4,6 +4,7 @@ import ReminderEmail from 'models/reminder-email/';
 
 import template from './template.stache!';
 import './email-confirm-modal.less!';
+import UserReq from 'utils/request/';
 
 var EmailConfirmModal = can.Component.extend({
   tag: 'rn-email-confirm-modal',
@@ -23,9 +24,55 @@ var EmailConfirmModal = can.Component.extend({
       this.element.remove();
     },
     '.submit click': function(el, ev) {
-      ReminderEmail.create({
-        approval: this.scope.approval
-      })
+     
+     var approvalObj = {"emailDetails":[  
+        {  
+           "emailType":"reminder",
+           "mailingList":{  
+              "groupId":null,
+              "roleId":null,
+              "to":[  
+                 "abc@apple.com"
+              ],
+              "bcc":null,
+              "cc":null
+           },
+           "attachments" : null,
+           "dynamicContents": {
+            "Subject": "Reminder for " + this.scope.approval.description,
+            "paymentBundle": this.scope.approval.description,
+            "noOfDaysPending": this.scope.approval.daysPending
+          }
+        }
+     ]};
+      
+      Promise.all([ReminderEmail.create(UserReq.formRequestDetails(approvalObj))]).then(function(values) {
+
+        if((typeof values[0] === "defined") && (values[0]["status"]=="SUCCESS")){
+
+               var msg = "Reminder was sent successfully."
+                $("#messageDiv").html("<label class='successMessage'>"+msg+"</label>")
+                $("#messageDiv").show();
+                setTimeout(function(){
+                    $("#messageDiv").hide();
+                 },5000)
+
+                
+                  //success or fail
+              }
+              else{
+                var msg = "Reminder was not sent successfully."
+                $("#messageDiv").html("<label class='errorMessage'>"+msg+"</label>")
+                $("#messageDiv").show();
+                setTimeout(function(){
+                    $("#messageDiv").hide();
+                 },5000)
+
+              }
+        
+
+      });
+
       this.element.find('.modal').modal('hide');
       this.element.remove();
     }
