28feec0cbf6a085c2427b55a2224327226cb0b94
ctsuser1 <ctsuser1@static-47.117.99.14-tataidc.co.in>
2015-01-25 19:47:44 +0530

    <rdar://problem/19261410> UI - Regular Invoice - Delete

--- components/page-invoices/page-invoices.js
+++ components/page-invoices/page-invoices.js
@@ -1005,7 +1005,7 @@ var page = Component.extend({
           BundleNamesModel.create(UserReq.formRequestDetails(overAllBundleRequest),function(data){
               console.log("passing params is "+JSON.stringify(data));
               if(data["status"]=="SUCCESS"){
-                if(data["responseCode"] == "IN1013" || data["responseCode"] == "IN1015"){
+                //if(data["responseCode"] == "IN1013" || data["responseCode"] == "IN1015"){
                  $("#messageDiv").html("<label class='successMessage' style='padding:3px 15px !important'>"+data["responseText"]+"</label>")
                  $("#messageDiv").show();
                  setTimeout(function(){
@@ -1018,7 +1018,7 @@ var page = Component.extend({
                         self.scope.appstate.attr('globalSearch', true);
                       }
                  },2000);
-               }
+               //}
               }
               else{
                 $("#messageDiv").html("<label class='errorMessage' style='padding:3px 15px !important'>"+data["responseText"]+"</label>")
