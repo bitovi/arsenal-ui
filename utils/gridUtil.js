f2adf7b9e4d08f8976a4f9c4856e22a9353b58ac
Peter <Peter@PeterMac.local>
2015-01-26 10:47:10 -0800

    Commented line removed

--- utils/gridUtil.js
+++ utils/gridUtil.js
@@ -17,7 +17,6 @@ var gridUtil = {
     var tableHeadheight=$('#'+element+' table>thead').height();
     var tableFootheight=$('#'+element+' table>tfoot').height();
     var tableBodyHeight=percentHeight-(tableHeadheight+tableFootheight+30); //30 pixel left out for the bottom space
-    console.log('WindowHiehgt',$('#'+element).offset().top,$(window).scrollTop(),$(window).height(),$("body").innerHeight(),posYOfelement,heightRemain,tableBodyHeight);
     return tableBodyHeight;
   }
 };
