98b0390d2215b07a583bce06334a4fe4430363a2
M <358311@PC271394.local>
2015-02-02 18:42:17 +0530

    Licensor Page: UI Fixes

--- components/page-licensor/page-licensor.js
+++ components/page-licensor/page-licensor.js
@@ -1672,7 +1672,6 @@ var page = Component.extend({
 
 
     ".applyAll click" : function(el, ev){
-
         var self = this;
 
         if(self.scope.submitAllReports == false) {
@@ -1930,6 +1929,10 @@ var page = Component.extend({
     ".applyAllSelected click" : function(el, ev){
       this.scope.mapCurrentCountryReportConf();
       this.scope.confirmSubmit();
+      setTimeout(function(){
+          alignGridLicenPop('repConfTable');
+      },100)
+      
     },
 
 
@@ -2221,5 +2224,60 @@ function alignGridStats(divId){
   }
 }
 
+function alignGridLicenPop(divId){
+  var colLength = $('#'+divId+' table>thead>tr>th').length;
+  var rowLength = $('#'+divId+' table>tbody>tr').length;
+  var divWidth = $('#'+divId).outerWidth();
+  var tableWidth = 0;
+  var tdWidth, cellWidthArr = [];
+  if(rowLength>0){
+    $('#'+divId+' table').css("width",divWidth-300);
+      for(var i=1;i<=colLength;i++){
+        var theadTdWidth = $('#'+divId+' table>thead>tr>th:nth-child('+i+')').outerWidth();
+        var tbodyTdWidth = $('#'+divId+' table>tbody>tr>td:nth-child('+i+')').outerWidth();
+        var tfootTdWidth = $('#'+divId+' table>tfoot>tr>td:nth-child('+i+')').outerWidth();
+
+        if(theadTdWidth >= tbodyTdWidth && theadTdWidth >= tfootTdWidth)
+          tdWidth = theadTdWidth;
+        else if(tfootTdWidth >= tbodyTdWidth && tfootTdWidth >= theadTdWidth)
+          tdWidth = tfootTdWidth;
+        else
+          if(divWidth > tableWidth + tbodyTdWidth) {
+            tdWidth = tbodyTdWidth;
+          } else {
+            tdWidth = divWidth - tableWidth;
+          }
+
+        if(i==1)
+          tdWidth = 45;
+        
+        tableWidth += tdWidth;
+        cellWidthArr.push(tdWidth);
+      }
+
+      if(tableWidth < divWidth){
+        var moreWidth = (divWidth-tableWidth)/colLength;
+        for(var j=1;j<=cellWidthArr.length;j++){
+          var width = cellWidthArr[j-1]+moreWidth;
+
+          $('#'+divId+' table>thead>tr>th:nth-child('+j+')').css("width",width);
+          $('#'+divId+' table>tbody>tr>td:nth-child('+j+')').css("min-width",width);
+          $('#'+divId+' table>tfoot>tr>td:nth-child('+j+')').css("width",width);
+        }
+        $('#'+divId+' table').css("width",divWidth);
+        $('#'+divId+' table>tbody').css("max-width",divWidth);
+      } else {
+        for(var j=1;j<=cellWidthArr.length;j++){
+          var width = cellWidthArr[j-1];
+          $('#'+divId+' table>thead>tr>th:nth-child('+j+')').css("width",width);
+          $('#'+divId+' table>tbody>tr>td:nth-child('+j+')').css("min-width",width);
+          $('#'+divId+' table>tfoot>tr>td:nth-child('+j+')').css("width",width);
+
+        }
+        $('#'+divId+' table').css("width",tableWidth);
+        $('#'+divId+' table>tbody').css("max-width",tableWidth);
+      }
+  }
+}
 
 export default page;
