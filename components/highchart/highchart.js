fa4d8a6091249055f49263b438b56d01b05e4eaf
Peter <Peter@PeterMac.local>
2015-02-02 12:57:17 -0800

    <rdar://problem/19682450> UI - Highchart - legend not visible .

--- components/highchart/highchart.js
+++ components/highchart/highchart.js
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import Component from 'can/component/';
 
 import template from './template.stache!';
@@ -19,184 +18,184 @@ var highchartpage = Component.extend({
   tag: 'high-chart',
   template: template,
   scope: {
-	  details:{},
-	  invoiceAmount:[]
+    details:{},
+    invoiceAmount:[]
   },
   init: function(){
-	 //console.log('Inside high chart');
-    },
-    events: {
-    	"inserted": function(){
+    //console.log('Inside high chart');
+  },
+  events: {
+    "inserted": function(){
 
-    		 var highChartdata;
-    		  	 var self = this;
+      var highChartdata;
+      var self = this;
 
-    		  	 var chartdata = self.scope.details;
-    		  	// console.log(JSON.stringify(self.scope.details));
+      var chartdata = self.scope.details;
+      // console.log(JSON.stringify(self.scope.details));
 
-    		  var genObj = {};
-    		     genObj["requestFrom"]=chartdata.requestFrom;
-    		     genObj["licensorId"]=chartdata.licensorId;
-    		     genObj["countryId"]=chartdata.countryId;
-    		     genObj["fiscalPeriod"]=chartdata.fiscalPeriod;
-    		     if(_.isString(chartdata.fiscalPeriod) && ( chartdata.fiscalPeriod.indexOf("P") > -1  || chartdata.fiscalPeriod.indexOf("Q") > -1)){
-					          genObj["fiscalPeriod"]=periodWidgetHelper.getFiscalPeriod(chartdata.fiscalPeriod);
-    		     }
-    		     genObj["periodType"]=chartdata.periodType;
-    		     genObj["contentType"]=chartdata.contentType;
-    		     console.log(" Data passed to service to fetch charts "+JSON.stringify(genObj));
-    		Promise.all([
-   	         HighChart.findOne(UserReq.formRequestDetails(genObj))
-   	     	 ]).then(function(values) {
-   	    	  var servicedata = values[0].historicalTrends;
-   	    	   		if(values[0].historicalTrends.length){
-   	    	   			highChartdata = prepareCanMap(values[0].historicalTrends);
-   	    	   			$("#chartContainer").addClass("highcharts_Overlay");
-   	    	   			$("#highChartDetails").removeClass("highcharts_Hide");
-			    		$('#highChartDetails').highcharts({
-			    			chart: {
-						        renderTo: 'highChartDetails',
-						        type: 'line',
-						        marginRight: 130,
-						        marginBottom: 25,
-						        events: {
-						            load: function() {
-						                this.renderer.image('resources/images/rn_RemoveFile@2x.png', 10, 10, 20, 20)
-						                .css({
-						                    cursor: 'pointer',
-						                    position: 'relative',
-						                    "margin-left": "0px",
-						                    opacity: 0.75
-						                }).attr({
-						                    id: 'close',
-						                    zIndex: -10
-						                }).add();
-						            }
-						        }
-						    },
+      var genObj = {};
+      genObj["requestFrom"]=chartdata.requestFrom;
+      genObj["licensorId"]=chartdata.licensorId;
+      genObj["countryId"]=chartdata.countryId;
+      genObj["fiscalPeriod"]=chartdata.fiscalPeriod;
+      if(_.isString(chartdata.fiscalPeriod) && ( chartdata.fiscalPeriod.indexOf("P") > -1  || chartdata.fiscalPeriod.indexOf("Q") > -1)){
+        genObj["fiscalPeriod"]=periodWidgetHelper.getFiscalPeriod(chartdata.fiscalPeriod);
+      }
+      genObj["periodType"]=chartdata.periodType;
+      genObj["contentType"]=chartdata.contentType;
+      console.log(" Data passed to service to fetch charts "+JSON.stringify(genObj));
+      Promise.all([
+        HighChart.findOne(UserReq.formRequestDetails(genObj))
+        ]).then(function(values) {
+          var servicedata = values[0].historicalTrends;
+          if(values[0].historicalTrends.length){
+            highChartdata = prepareCanMap(values[0].historicalTrends);
+            $("#chartContainer").addClass("highcharts_Overlay");
+            $("#highChartDetails").removeClass("highcharts_Hide");
+            $('#highChartDetails').highcharts({
+              chart: {
+                renderTo: 'highChartDetails',
+                type: 'line',
+                //marginRight: 130,
+                marginBottom: 100,
+                events: {
+                  load: function() {
+                    this.renderer.image('resources/images/rn_RemoveFile@2x.png', 10, 10, 20, 20)
+                    .css({
+                      cursor: 'pointer',
+                      position: 'relative',
+                      "margin-left": "0px",
+                      opacity: 0.75
+                    }).attr({
+                      id: 'close',
+                      zIndex: -10
+                    }).add();
+                  }
+                }
+              },
 
-					        title: {
-					            //text: servicedata[0].countryName+"-"+servicedata[0].licensorName,
-					        	text: chartdata.countryId+"-"+chartdata.licensorId,
-					            x: -20 //center
-					        },
-					        subtitle: {
-					           // text: 'Source: WorldClimate.com',
-					            x: -20
-					        },
-					        credits: {
-    						  enabled: false
-  							},
-					        xAxis: {
-					            title: {
-						                    text: 'Period',
-						                    align: 'middle'
-						                },
-					            categories: highChartdata["FISCAL_PERIOD"],
-					              labels: {
-										//rotation: -45
-									}
-					        },
-					        yAxis: {
-					            title: {
-					                text: 'Amount'
-					            },
-					            plotLines: [{
-					                value: 0,
-					                width: 1,
-					                color: '#FFFFFF'
-					            }]
-					        },
-					        tooltip: {
-					        	formatter: function () {
-									return '<b>' + this.series.name + '</b><br/>' + this.x + ': ' + currencyFormat(this.y) +' USD';
-								}
-					        },
-					        legend: {
-					            layout: 'vertical',
-					            align: 'right',
-					            verticalAlign: 'middle',
-					            borderWidth: 0
-					        },
-					        exporting: {
-					            enabled: false
-					        },
-					        series: [{
-					            name: 'Invoice Amount',
-					            data: highChartdata["INVOICE_AMOUNT"]
-					        }, {
-					            name: 'Overrep Amount',
-					            data: highChartdata["OVERREP_AMOUNT"]
-					        }, {
-					            name: 'Line item dispute',
-					            data: highChartdata["LINE_ITEM_AMOUNT"]
-					        }]
-					    });
+              title: {
+                //text: servicedata[0].countryName+"-"+servicedata[0].licensorName,
+                text: chartdata.countryId+"-"+chartdata.licensorId,
+                x: -20 //center
+              },
+              subtitle: {
+                // text: 'Source: WorldClimate.com',
+                x: -20
+              },
+              credits: {
+                enabled: false
+              },
+              xAxis: {
+                title: {
+                  text: 'Period',
+                  align: 'middle'
+                },
+                categories: highChartdata["FISCAL_PERIOD"],
+                labels: {
+                  rotation: -35
+                }
+              },
+              yAxis: {
+                title: {
+                  text: 'Amount'
+                },
+                plotLines: [{
+                  value: 0,
+                  width: 1,
+                  color: '#FFFFFF'
+                }]
+              },
+              tooltip: {
+                formatter: function () {
+                  return '<b>' + this.series.name + '</b><br/>' + this.x + ': ' + currencyFormat(this.y) +' USD';
+                }
+              },
+              legend: {
+                layout: 'horizontal',
+                align: 'center',
+                verticalAlign: 'bottom',
+                borderWidth: 0
+              },
+              exporting: {
+                enabled: false
+              },
+              series: [{
+                name: 'Invoice Amount',
+                data: highChartdata["INVOICE_AMOUNT"]
+              }, {
+                name: 'Overrep Amount',
+                data: highChartdata["OVERREP_AMOUNT"]
+              }, {
+                name: 'Line item dispute',
+                data: highChartdata["LINE_ITEM_AMOUNT"]
+              }]
+            });
 
-					}else{
-						console.log('high chart did not return any data');
-						$("#highChartDetails").addClass("highcharts_Hide");
-						 $("#messageDiv").html("<label class='errorMessage'>Data not available</label>");
-						 //$("#chartContainer").removeClass('highcharts_Overlay');
-			             $("#messageDiv").show();
-			             setTimeout(function(){
-			                $("#messageDiv").hide();
-			             },2000);
-					}
-						// $("#highChartDetails").resizable({
-						// 	containment: 'document'
-						// });
-			 });
-    	},
-    	'{document} keyup' : function(el,ev){
-	      if (ev.keyCode == 27){
-	        $("#highChartDetails").addClass("highcharts_Hide");
-        	$("#chartContainer").removeClass('highcharts_Overlay');
-	      }
-	    }
+          }else{
+            console.log('high chart did not return any data');
+            $("#highChartDetails").addClass("highcharts_Hide");
+            $("#messageDiv").html("<label class='errorMessage'>Data not available</label>");
+            //$("#chartContainer").removeClass('highcharts_Overlay');
+            $("#messageDiv").show();
+            setTimeout(function(){
+              $("#messageDiv").hide();
+            },2000);
+          }
+          // $("#highChartDetails").resizable({
+          // 	containment: 'document'
+          // });
+        });
+      },
+      '{document} keyup' : function(el,ev){
+        if (ev.keyCode == 27){
+          $("#highChartDetails").addClass("highcharts_Hide");
+          $("#chartContainer").removeClass('highcharts_Overlay');
+        }
+      }
     },
     "#myImage click ":function(){
-    	$("#highChartDetails").addClass("highcharts_Hide");
+      $("#highChartDetails").addClass("highcharts_Hide");
     }
-});
+  });
 
 
-function prepareCanMap(object){
-	console.log(object);
-	console.log("inside prepare map ----"+object.length);
-	var highChartdata = new Array();
-	var periodList=[];
-	var invoiceAmountList = [];
-	var overRepAmountList = [];
-	var lineItemAmountList = [];
-	for(var i=0; i<object.length;i++){
-		var obj = object[i];
-		console.log("Inside for loop :"+obj.fiscalPeriod);
-		 periodList[i] = periodWidgetHelper.getDisplayPeriod(obj.fiscalPeriod, "P");//obj.fiscalPeriod;
-		 invoiceAmountList[i] = obj.invoiceAmount;
-		 overRepAmountList[i] = obj.overRepAmount;
-		 lineItemAmountList[i] = obj.lineItemsAmount;
-	}
-	console.log("chart data:"+periodList);
-	highChartdata["FISCAL_PERIOD"] = periodList;
-	highChartdata["INVOICE_AMOUNT"] = invoiceAmountList;
-	highChartdata["OVERREP_AMOUNT"] = overRepAmountList;
-	highChartdata["LINE_ITEM_AMOUNT"] = lineItemAmountList;
-	//console.log("chart data:"+highChartdata["LINE_ITEM_AMOUNT"]);
-	return highChartdata;
-}
-
-function currencyFormat(number)
-  {
-    if(number != undefined && $.isNumeric(number)){
-      if(typeof(number) == 'string'){
-        number = Number(number);
-      }
-      var n = number.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,");
-      return n;
-    }else{
-      return 0;
+  function prepareCanMap(object){
+    console.log(object);
+    console.log("inside prepare map ----"+object.length);
+    var highChartdata = new Array();
+    var periodList=[];
+    var invoiceAmountList = [];
+    var overRepAmountList = [];
+    var lineItemAmountList = [];
+    for(var i=0; i<object.length;i++){
+      var obj = object[i];
+      console.log("Inside for loop :"+obj.fiscalPeriod);
+      periodList[i] = periodWidgetHelper.getDisplayPeriod(obj.fiscalPeriod, "P");//obj.fiscalPeriod;
+      invoiceAmountList[i] = obj.invoiceAmount;
+      overRepAmountList[i] = obj.overRepAmount;
+      lineItemAmountList[i] = obj.lineItemsAmount;
     }
+    console.log("chart data:"+periodList);
+    highChartdata["FISCAL_PERIOD"] = periodList;
+    highChartdata["INVOICE_AMOUNT"] = invoiceAmountList;
+    highChartdata["OVERREP_AMOUNT"] = overRepAmountList;
+    highChartdata["LINE_ITEM_AMOUNT"] = lineItemAmountList;
+    //console.log("chart data:"+highChartdata["LINE_ITEM_AMOUNT"]);
+    return highChartdata;
   }
+
+  function currencyFormat(number)
+{
+  if(number != undefined && $.isNumeric(number)){
+    if(typeof(number) == 'string'){
+      number = Number(number);
+    }
+    var n = number.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,");
+    return n;
+  }else{
+    return 0;
+  }
+}
 
 export default highchartpage;
