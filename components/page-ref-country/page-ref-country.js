8f3599ae89135b7b8f2fdd42cc6f3bb0f2ba877f
sreeram_rajendran <sreeram_rajendran@apple.com>
2015-02-02 17:10:38 +0530

    <radar:19657687>

--- components/page-ref-country/page-ref-country.js
+++ components/page-ref-country/page-ref-country.js
@@ -126,7 +126,7 @@ var page = Component.extend({
       $(".buttonsPlaceHolder").hide();
 
       $('#grid-society-model').append(stache('<rn-grid rows="{reportConfigurationList}"></rn-grid>')({reportConfigurationList}));
-      $('#grid-revision-history').append(stache('<rn-grid-revision-history-country rows="{revisionHistory}"></rn-grid-revision-history-country>')({revisionHistory}));
+      $('#grid-revision-history-country').append(stache('<rn-grid-revision-history-country rows="{revisionHistory}"></rn-grid-revision-history-country>')({revisionHistory}));
 
 
       $('#countryForm').on('init.form.bv', function(e, data) {
@@ -213,7 +213,7 @@ var page = Component.extend({
     '.updatePeroid focus':function(el){
         $(el).closest('.calendarcls').find('.box-modal').show().trigger( "focus" );
      },
-    "#grid-revision-history table>tbody>tr click": function(item, el, ev){
+    "#grid-revision-history-country table>tbody>tr click": function(item, el, ev){
         //var invoiceid = el.closest('tr').data('row').row.id;
         var self=this.scope;
         $("#loading_img1").show();
@@ -355,7 +355,12 @@ var page = Component.extend({
     '{revisionHistory} change': function() {
       var self = this;
       var revisionHistory = self.scope.revisionHistory;
-        $('#grid-revision-history').html(stache('<rn-grid-revision-history-country  rows="{revisionHistory}"></rn-grid-revision-history-country>')({revisionHistory}));
+      $('#grid-revision-history-country').html(stache('<rn-grid-revision-history-country  rows="{revisionHistory}"></rn-grid-revision-history-country>')({revisionHistory}));
+      
+      setTimeout(function(){
+        alignGrid("grid-revision-history-country");
+        alignGrid("grid-society-model");
+      },50);
     },
     '{societyModelMapping} change': function() {
       var self = this;
@@ -787,6 +792,56 @@ var showErrorMsg = function(periodFrom,periodTo,whichcomp){
         if(showFlg==true){ $('.period-invalid').show(); return false;}else {showFlg=false; $('.period-invalid').hide();}
     }
 
+
+function alignGrid(divId){
+  var colLength = $('#'+divId+' table>thead>tr>th').length;
+  var rowLength = $('#'+divId+' table>tbody>tr').length;
+  var divWidth = $('#'+divId).outerWidth();
+  var tableWidth = 0;
+  var tdWidth, cellWidthArr = [];
+  if(rowLength>0){
+    $('#'+divId+' table').css("width",divWidth-300);
+      for(var i=1;i<=colLength;i++){
+        var theadTdWidth = $('#'+divId+' table>thead>tr>th:nth-child('+i+')').outerWidth();
+        var tbodyTdWidth = $('#'+divId+' table>tbody>tr>td:nth-child('+i+')').outerWidth();
+        var tfootTdWidth = $('#'+divId+' table>tfoot>tr>td:nth-child('+i+')').outerWidth();
+
+        if(theadTdWidth >= tbodyTdWidth && theadTdWidth >= tfootTdWidth)
+          tdWidth = theadTdWidth;
+        else if(tfootTdWidth >= tbodyTdWidth && tfootTdWidth >= theadTdWidth)
+          tdWidth = tfootTdWidth;
+        else 
+          tdWidth = tbodyTdWidth;
+
+        if(i==1)
+          tdWidth = 35;
+        
+        tableWidth += tdWidth;
+        cellWidthArr.push(tdWidth);
+      }
+
+      if(tableWidth < divWidth){
+        var moreWidth = (divWidth-tableWidth)/colLength;
+        for(var j=1;j<=cellWidthArr.length;j++){
+          var width = cellWidthArr[j-1]+moreWidth;
+
+          $('#'+divId+' table>thead>tr>th:nth-child('+j+')').css("width",width);
+          $('#'+divId+' table>tbody>tr>td:nth-child('+j+')').css("width",width);
+          $('#'+divId+' table>tfoot>tr>td:nth-child('+j+')').css("width",width);
+        }
+        $('#'+divId+' table').css("width",divWidth);
+      } else {
+        for(var j=1;j<=cellWidthArr.length;j++){
+          var width = cellWidthArr[j-1];
+          $('#'+divId+' table>thead>tr>th:nth-child('+j+')').css("width",width);
+          $('#'+divId+' table>tbody>tr>td:nth-child('+j+')').css("width",width);
+          $('#'+divId+' table>tfoot>tr>td:nth-child('+j+')').css("width",width);
+
+        }
+        $('#'+divId+' table').css("width",tableWidth);
+      }
+  }
+}
 
   
 
