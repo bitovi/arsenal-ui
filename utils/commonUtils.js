63c5794766e28fa3145a7cca21a1f0287cab4e11
ajith <analla@apple.com>
2015-02-11 19:03:45 -0800

    CommonChanges UI: Added common method

--- utils/commonUtils.js
+++ utils/commonUtils.js
@@ -22,52 +22,16 @@ var rinsCommonUtils = {
   },
   hideUIMessage: function(){
     var messageDiv = ".messageDiv";
-    
+
     if($(messageDiv).is(':visible')){
       $(messageDiv).html("").hide();
     }
   },
-  displayUIMessage : function(statusCode,message){
-
-    var className = 'errorMessage';
-    if(statusCode === '0000' ||  statusCode === 'SUCCESS' ){
-      className= 'successMessage';
-    }
-    var messageDiv = ".messageDiv";
-
-    if($(messageDiv).is(':visible')){
-      $(messageDiv).html("").hide();
-    }
-    $(messageDiv).html("<label class='"+className+"'>"+message+"<a href='#' id='messageClose' class='close messageClose'> &times;</a></label>").show();
-
-    if($(".messageClose").is(':visible')){
-      $(".messageClose").on("click", function(){
-        $(messageDiv).hide();
-      });
-    }
+  displayUIMessage : function(status,message){
+    commonUIDisplay(  ".messageDiv", status,message);
   },
   displayUIMessageWithDiv : function(messageDiv, status,message){
-    var className = 'errorMessage';
-    if(status === 'SUCCESS'){
-      className= 'successMessage';
-    }
-    // $(messageDiv).html("<label class='"+className+"' style='padding: 0px 15px;'>"+message+"</label>")
-    // $(messageDiv).show();
-    //
-    // setTimeout(function(){
-    //   $(messageDiv).hide();
-    // },constants.MESSAGE_DISPLAY_TIME);
-
-    if($(messageDiv).is(':visible')){
-      $(messageDiv).html("").hide();
-    }
-    $(messageDiv).html("<label class='"+className+"'>"+message+"<a href='#' id='messageClose' class='close messageClose'> &times;</a></label>").show();
-
-    if($(".messageClose").is(':visible')){
-      $(".messageClose").on("click", function(){
-        $(messageDiv).hide();
-      });
-    }
+    commonUIDisplay(messageDiv, status,message);
   },
   getDefaultParameters:function(appstate){
       var defaultFilterData={};
@@ -108,6 +72,26 @@ var rinsCommonUtils = {
 
 
 };
+
+var   commonUIDisplay  = function(messageDiv, status,message){
+
+  var className = 'errorMessage';
+  if(status === '0000' ||  status === 'SUCCESS' ){
+    className= 'successMessage';
+  }
+
+
+  if($(messageDiv).is(':visible')){
+    $(messageDiv).html("").hide();
+  }
+  $(messageDiv).html("<label class='"+className+"'>"+message+"<a href='#' id='messageClose' class='close messageClose'> &times;</a></label>").show();
+
+  if($(".messageClose").is(':visible')){
+    $(".messageClose").on("click", function(){
+      $(messageDiv).hide();
+    });
+  }
+}
 
 
 export default rinsCommonUtils;
