0bf23ff359598d585f35f3073c3fd2cf4cc9e21e
ctsuser1 <ctsuser1@LT020754.local>
2015-02-19 15:31:29 +0530

    <rdar://problem/19843999> UI : Wave M2 : Search Invoices - Adhoc invoices are getting listed with contenttype selection

--- components/global-parameter-bar/global-parameter-bar.js
+++ components/global-parameter-bar/global-parameter-bar.js
@@ -273,7 +273,7 @@ var GlobalParameterBar = Component.extend({
       setTimeout(function(){
         if($("input[name='selAllContentType']").prop("checked"))
         {
-          self.scope.allContentTypes.attr().lenght() > 1 ? self.scope.changesToApply.attr('contentType', ["-1"]) : "";
+          self.scope.allContentTypes.length > 1 ? self.scope.changesToApply.attr('contentType', ["-1"]) : "";
         }
         else if($(el[0]).val() == null)
         {
