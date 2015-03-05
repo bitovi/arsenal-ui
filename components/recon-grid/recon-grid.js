d332e1e360fb1476c5311cf55b65a50b017fa72d
ajith <analla@apple.com>
2015-03-04 18:15:30 -0800

    Recon: Un wanted code  / commented removed

--- components/recon-grid/recon-grid.js
+++ components/recon-grid/recon-grid.js
@@ -28,7 +28,7 @@ var reconGrid = Grid.extend({
         var tableScrollTopVal = parentScopeVar.attr('ingestedScrollTop');
         $(tbody[0]).scrollTop(tableScrollTopVal);
           $(tbody).on('scroll', function(ev) {
-            if(tbody[0].scrollTop + tbody[0].clientHeight >= tbody[0].scrollHeight && parentScopeVar.recordsAvailable) {
+            if(parentScopeVar.recordsAvailable && tbody[0].scrollTop + tbody[0].clientHeight >= tbody[0].scrollHeight) {
               //console.log(JSON.stringify(self.element.closest('page-invoices').scope().appstate.attr()));
 
               var tabSelected = parentScopeVar.attr('tabSelected');
@@ -135,55 +135,5 @@ var reconGrid = Grid.extend({
 
   }
 });
-
-function alignGrid(divId){
-  /*var colLength = $('#'+divId+' table>thead>tr>th').length;
-  var rowLength = $('#'+divId+' table>tbody>tr').length;
-  var divWidth = $('#'+divId).outerWidth();
-  var tableWidth = 0;
-  var tdWidth, cellWidthArr = [];
-  if(rowLength>0){
-    $('#'+divId+' table').css("width",divWidth-300);
-      for(var i=1;i<=colLength;i++){
-        var theadTdWidth = $('#'+divId+' table>thead>tr>th:nth-child('+i+')').outerWidth();
-        var tbodyTdWidth = $('#'+divId+' table>tbody>tr>td:nth-child('+i+')').outerWidth();
-        var tfootTdWidth = $('#'+divId+' table>tfoot>tr>td:nth-child('+i+')').outerWidth();
-
-        if(theadTdWidth >= tbodyTdWidth && theadTdWidth >= tfootTdWidth)
-          tdWidth = theadTdWidth;
-        else if(tfootTdWidth >= tbodyTdWidth && tfootTdWidth >= theadTdWidth)
-          tdWidth = tfootTdWidth;
-        else
-          tdWidth = tbodyTdWidth;
-
-        if(i==1)
-          tdWidth = 35;
-
-        tableWidth += tdWidth;
-        cellWidthArr.push(tdWidth);
-      }
-
-      if(tableWidth < divWidth){
-        var moreWidth = (divWidth-tableWidth)/colLength;
-        for(var j=1;j<=cellWidthArr.length;j++){
-          var width = cellWidthArr[j-1]+moreWidth;
-
-          $('#'+divId+' table>thead>tr>th:nth-child('+j+')').css("width",width);
-          $('#'+divId+' table>tbody>tr>td:nth-child('+j+')').css("width",width);
-          $('#'+divId+' table>tfoot>tr>td:nth-child('+j+')').css("width",width);
-        }
-        $('#'+divId+' table').css("width",divWidth);
-      } else {
-        for(var j=1;j<=cellWidthArr.length;j++){
-          var width = cellWidthArr[j-1];
-          $('#'+divId+' table>thead>tr>th:nth-child('+j+')').css("width",width);
-          $('#'+divId+' table>tbody>tr>td:nth-child('+j+')').css("width",width);
-          $('#'+divId+' table>tfoot>tr>td:nth-child('+j+')').css("width",width);
-
-        }
-        $('#'+divId+' table').css("width",tableWidth);
-      }
-  }*/
-}
 
 export default reconGrid;
