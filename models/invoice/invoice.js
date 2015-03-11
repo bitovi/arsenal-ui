3d9f0c10d98293f3c65d321d17dcc1fc31c1d2a7
santosh_giridhara <santosh_giridhara@apple.com>
2015-03-11 19:25:20 +0530

    Fixed icsv validation: <rdar://problem/20013106> Error does not show in iCSV until Submit is pressed

--- models/invoice/invoice.js
+++ models/invoice/invoice.js
@@ -20,6 +20,15 @@ var Invoice = Model.extend({
   		data: JSON.stringify(params)
   	})
   },
+  validate: function(params){
+  return $.ajax({
+      url: RinsCommon.DOMAIN_SERVICE_URL+'invoice/validate',
+      type: 'POST',
+      datatype:'json',
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(params)
+    })
+  },
   update: function(params,type){
     if(type=="invoiceDelete"){
      return $.ajax({
